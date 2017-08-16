02910f197e6c551db2868679647d9199bbd2f761
Cristiano Antunes de Souza Caldeira <cristianocachi@gmail.com>
2017-08-15 22:06:37 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,17 @@
+angular.module('ionicApp', ['ionic'])
+
+.controller("MainCtrl", function(){
+	console.log("Main Controller Says: Hello World");
+	})
+	
+	.config(function($stateProvider, $urlRouterProvider){
+		$stateProvider
+		
+		.state('main', {
+			url: "/main",
+			templateUrl: "templates/main.html",
+			controller: 'MainCtrl'
+		})
+		
+		$urlRouterProvider.otherwise('/main');
+	});
